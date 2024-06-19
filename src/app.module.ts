a38e701b65920b85facae69363435e0855c2762e
ruyi <jpretamalesv@gmail.com>
2024-06-19 18:33:54 -0400

    FIX: axios to observable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HttpModule } from '@nestjs/axios';
 import { UsersPostsModule } from './users-posts/users-posts.module';
 
 @Module({
-  imports: [HttpModule, UsersPostsModule],
+  imports: [UsersPostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users-posts/users-posts.service.ts
+++ src/users-posts/users-posts.service.ts
@@ -1,7 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { AxiosError, AxiosResponse } from 'axios';
-import { Observable, catchError, forkJoin, map, of, tap } from 'rxjs';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { Observable, catchError, forkJoin, from, map, of } from 'rxjs';
 import { IPost, IUser, IUsersPosts } from './interface/jsonplaceholder-response.interface';
 
 
@@ -10,8 +10,8 @@ export class UsersPostsService {
     constructor(private readonly _http: HttpService) { }
     getUserPost(): Observable<IUsersPosts> {
 
-        const users$ = this.buildRequest('users');
-        const posts$ = this.buildRequest('posts');
+        const users$: Observable<IUser[]> = this.buildRequest('users');
+        const posts$: Observable<IPost[]>= this.buildRequest('posts');
 
         return forkJoin({ users: users$, posts: posts$ }).pipe(
           map(({ users, posts }) => {
@@ -32,7 +32,7 @@ export class UsersPostsService {
     
       buildRequest(endpoint: string): Observable<IUser[] & IPost[]> {
         const baseUrl: string = 'https://jsonplaceholder.typicode.com';
-        return this._http.get<IUser[] | IPost[]>(baseUrl + `/${endpoint}`).pipe(
+        return from(axios.get<IUser[] | IPost[]>(baseUrl + `/${endpoint}`)).pipe(
           map(({ data }: AxiosResponse) => data),
           catchError((error: AxiosError) => of({ status: error.status, error: error.message, detail: `Error fetching ${endpoint}` })));
       }
