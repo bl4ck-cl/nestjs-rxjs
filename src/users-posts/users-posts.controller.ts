871f03b324b567e58aceff1ee13fa5bc44ac31f3
ruyi <jpretamalesv@gmail.com>
2024-06-19 18:26:32 -0400

    FIX: response typos

--- src/users-posts/users-posts.controller.ts
+++ src/users-posts/users-posts.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersPostsService } from './users-posts.service';
 import { Observable } from 'rxjs';
+import { IUsersPosts } from './interface/jsonplaceholder-response.interface';
 
 @Controller('users-posts')
 export class UsersPostsController {
 constructor(private readonly appService: UsersPostsService) {}
     
   @Get('')
-  getUserPost(): Observable<any> {
+  getUserPost(): Observable<IUsersPosts> {
     return this.appService.getUserPost();
   }
 }
